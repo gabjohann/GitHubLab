262f88509a9b71da1c8be36c68e9052efdf19ec4
Lucas Gabriel Fernandes Johann <lukito@pop-os.localdomain>
2022-04-11 20:06:22 -0300

    instalando styled components

--- ignitemoney/src/App.tsx
+++ ignitemoney/src/App.tsx
@@ -1,8 +1,16 @@
+import styled from 'styled-components';
+
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;
+`
+
 // exportar desta forma não permite mudar o nome da função na importação
 export function App() {
   return (
     <div className="App">
-       <h1>Hello World!</h1>
+       <Title>Hello World!</Title>
     </div>
   );
 } 
