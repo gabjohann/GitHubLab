eaeaed3e7c8a2fa2c406da8dd5f9233c5be36176
Lucas Gabriel Fernandes Johann <lukito@pop-os.localdomain>
2022-04-10 23:18:22 -0300

    estrutura com create-react-app

---
+++ comandList.md
@@ -0,0 +1,12 @@
+# Lista de Comandos Úteis
+
+## Linux 
+
+- Fechar um programa travado `sudo xkill`
+
+
+## Git 
+
+## JavaScript 
+
+- Executar scripts direto no terminal `node scriptName.js`
---
+++ ignitemoney/src/App.tsx
@@ -0,0 +1,8 @@
+// exportar desta forma não permite mudar o nome da função na importação
+export function App() {
+  return (
+    <div className="App">
+       <h1>Hello World!</h1>
+    </div>
+  );
+} 
