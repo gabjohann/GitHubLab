977a9c28470697a2330b9b52d2c4aaae63608b7f
Lucas Gabriel Fernandes Johann <lukito@pop-os.localdomain>
2022-04-10 22:44:46 -0300

    aula 07 - loops

--- main.js
+++ main.js
@@ -1,26 +1,57 @@
-// JSON
+// Loops
 
-const user = [
-  {
-    id: 1, 
-    name: 'Lucas', 
-    age: 19
-  },
-  {
-    id: 2, 
-    name: 'Gabriel', 
-    age: 20
-  },
-  {
-    id: 1, 
-    name: 'Anakin', 
-    age: 22
-  },
+// index inicia com valor 0 e é somado 1 enquanto o index for menor que 10
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+const series = [
+  'Once Upon a Time', 
+  'Station 19', 
+  'Orange is The New Black', 
+  'Atypical'
 ];
 
-// conversão para JSON
-const userJSON = JSON.stringify(user);
 
-// conversão para listas
-const userList = JSON.parse(userJSON);
-console.log(userList);
+//for
+for (let i = 0; i < series.length; i++) {
+  console.log(series[i]);
+}  // se fosse somente console.log(series) iria retornar uma lista
+
+
+// for of
+for (let serie of series) {
+  console.log(serie);
+}
+ 
+
+// for each
+series.forEach(function (serie) {
+  console.log(serie);
+});
+
+// for each permite ver o index
+series.forEach(function (serie, index) {
+  console.log(index + ' -> '+ serie);
+});
+
+
+// while 
+
+let index = 0;
+
+while (index < 10) {
+  console.log('index é menor do que 10');
+  index++; // index += 1 || index = index + 1;
+}
+
+// for in -> utilizado em objetos
+
+const person = {
+  name: 'Lucas',
+  age: 19
+}
+
+for (property in person) {
+  console.log(person[property]);
+}
