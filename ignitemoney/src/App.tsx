4ff35ebc27fb5b8e6fcd713125fe78244aa4b039
Lucas Gabriel <gabif.johann@gmail.com>
2022-04-11 23:04:54 -0300

    componente: header

--- ignitemoney/src/App.tsx
+++ ignitemoney/src/App.tsx
@@ -1,11 +1,13 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 // exportar desta forma não permite mudar o nome da função na importação
 export function App() {
   return (
-    <div className="App">
-       <h1>Hello World!</h1>
+    <>
+       <Header/>
        <GlobalStyle/>
-    </div>
+    </>
   );
 } 
+ 
