279ebcc151de58b95e6422baa623d2b390a6bab1
Lucas Gabriel <gabif.johann@gmail.com>
2022-04-16 22:38:51 -0300

    aula 13 - manipulando elementos do dom

--- main.js
+++ main.js
@@ -1,44 +1,16 @@
-// Selecionar Elementos da DOM 
+// Manipulando elementos da DOM
 
-// selecionar somente um elemento 
+const items = document.querySelector('.items');
+console.log(items);
 
-// getElementById retorna uma referência ao elemento 
-const addUserText = document.getElementById('add-user');
-console.log(addUserText); // retorna ao que esta atrelado este ID
+/* items.remove(); // remove todos os items 
+items.firstElementChild.remove(); // remove o primeiro item
+items.lastElementChild.remove(); // remove o último item */
 
-addUserText.innerText = 'Adicionar usuário'; // troca o dado de H1
+items.children[0].textContent = 'Primeiro item'; // muda o texto do elemento selecionado
 
-// querySelector retorna o elemento 
-const addUserText2 = document.querySelector('#add-user'); // # significa que é um ID
-addUserText2.textContent = 'Adicionar usuário'; // troca o dado de H1
-console.log(addUserText2); 
+items.lastElementChild.innerHTML = '<h2>Mudei</h2>'; // muda o HTML 
 
-// getElementById não permitira selecionar este elemento 
-const myForm = document.querySelector(".container #my-form");
-console.log(myForm);
+const button = document.querySelector('.btn'); 
 
-
-// selecionar múltiplos elementos 
-
-const allItems = document.querySelectorAll('.item');
-console.log(allItems);
-//permite selecionar um item da lista
-//console.log(allItems[1]);
-
-// retorna um HTMLCollection, o qual não permite o uso de métodos de listas
-const allItems2 = document.getElementsByClassName('item');
-console.log(allItems2);
-
-const allItems3 = document.getElementsByTagName('li');
-console.log(allItems3);
-
-
-const addUserText = document.getElementById('add-user');
-console.log(addUserText); // retorna ao que esta atrelado este ID
-
-
-addUserText.innerText = 'Adicionar usuário'; // troca o dado de H1
-
-
-const addUserText = document.querySelector('#add-user'); // # significa que é um ID
-console.log(addUserText); // retorna ao que est
+button.style.background =  '#836FFF'; // muda a cor do botão
