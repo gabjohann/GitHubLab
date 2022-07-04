ecced0189ef90e7dbd5fbd66124c134d6df0a08d
Lucas Gabriel <gabif.johann@gmail.com>
2022-07-04 00:03:38 -0300

    movendo criacao para o context

--- ignitemoney/src/TransactionsContext.tsx
+++ ignitemoney/src/TransactionsContext.tsx
@@ -10,12 +10,32 @@ interface Transaction {
   createdAt: string;
 }
 
+// Forma 1
+/* interface TransactionInput {
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+} */
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+// Forma 3
+// type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category'>
+
 // ReactNode -> aceita qualquer tipo de conteúdo válido pelo ReactJS
 interface TransactionsProviderProps {
   children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData // força tipagem
+);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -25,8 +45,13 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then(response => setTransactions(response.data.transactions));
   }, []);
 
+  function createTransaction(transaction: TransactionInput) {
+
+    api.post('/transactions', transaction);
+  }
+
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
       { children }
     </TransactionsContext.Provider>
   );
