5e4304e6d8cb93810946a7ca605f70fb13a829d8
Lucas Gabriel <gabif.johann@gmail.com>
2022-07-04 00:38:15 -0300

    criano hook

--- ignitemoney/src/hooks/useTransactions.tsx
+++ ignitemoney/src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState, ReactNode } from "react";
-import { api } from "./services/api";
+import { createContext, useEffect, useState, ReactNode, useContext } from "react";
+import { api } from "../services/api";
 
 interface Transaction {
   id: number;
@@ -33,7 +33,7 @@ interface TransactionsContextData {
   createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData // força tipagem
 );
 
@@ -65,4 +65,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       { children }
     </TransactionsContext.Provider>
   );
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
